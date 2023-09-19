78683f5ecc148505d7c42f37b4b2951707c86dfd
Milush47 <kulpievmilush@gmail.com>
2023-09-19 20:50:25 +0200

    ADDED:  - extra practice. plots for two more months

--- Practice/ClimateChange/emission_to_temperature_correlation.r
+++ Practice/ClimateChange/emission_to_temperature_correlation.r
@@ -54,3 +54,71 @@ mtext(side = 5, line = 3, "CO2 emissions")
 
 legend("topleft", legend = c("June temp anom", "CO2 emis"),
        lty = c(1, 1), col = c("blue", "black"), lwd = 2)
+
+#EXTRA PRACTISE
+#plots for January and September
+CO2data_january <- CO2data[CO2data$Month == 1, ]
+CO2data_september <- CO2data[CO2data$Month == 9, ]
+
+tempCO2data <- merge(tempdata, CO2data_january)
+tempCO2data <- merge(tempdata, CO2data_september)
+
+head(tempCO2data[, c("Year", "Jan", "Trend")])
+
+tempCO2data$Jan <- ts(tempCO2data$Jan,
+    start = c(1958), end = c(2016), frequency = 1
+)
+
+tempCO2data$Sep <- ts(tempCO2data$Sep,
+    start = c(1958), end = c(2016), frequency = 1
+)
+
+tempCO2data$Trend <- ts(tempCO2data$Trend,
+    start = c(1958), end = c(2016), frequency = 1
+)
+
+par(mar = c(5, 5, 2, 5))
+
+#JANUARY
+plot(tempCO2data$Jan, type = "l", col = "blue", lwd = 2,
+     ylab = "January temperature anomalies", xlab = "Year")
+
+title("January temperature anomalies and CO2 emissions")
+
+par(new = T)
+
+plot(tempCO2data$Trend,
+    pch = 16, lwd = 2,
+    axes = FALSE, xlab = NA, ylab = NA, cex = 1.2
+)
+
+axis(side = 4)
+mtext(side = 5, line = 3, "CO2 emissions")
+
+legend("topleft",
+    legend = c("January temp anom", "CO2 emis"),
+    lty = c(1, 1), col = c("blue", "black"), lwd = 2
+)
+
+#SEPTEMBER
+plot(tempCO2data$Sep,
+    type = "l", col = "blue", lwd = 2,
+    ylab = "Semptember temperature anomalies", xlab = "Year"
+)
+
+title("Semptember temperature anomalies and CO2 emissions")
+
+par(new = T)
+
+plot(tempCO2data$Trend,
+    pch = 16, lwd = 2,
+    axes = FALSE, xlab = NA, ylab = NA, cex = 1.2
+)
+
+axis(side = 4)
+mtext(side = 5, line = 3, "CO2 emissions")
+
+legend("topleft",
+    legend = c("Semptember temp anom", "CO2 emis"),
+    lty = c(1, 1), col = c("blue", "black"), lwd = 2
+)
