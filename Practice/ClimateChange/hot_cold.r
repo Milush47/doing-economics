0cfa2ed805a51690234a37e97169f9b513af0271
Milush47 <kulpievmilush@gmail.com>
2023-09-19 18:15:20 +0200

    ADDED:  -  % of cold and hot monthes in 1981-2010 period

--- Practice/ClimateChange/hot_cold.r
+++ Practice/ClimateChange/hot_cold.r
@@ -16,3 +16,20 @@ p30
 # The hot threshold
 p70 <- perc[2]
 p70
+
+# Select years 1981 to 2010
+temp_all_months <- subset(tempdata, (Year >= 1981 & Year <= 2010))
+
+# Columns 2 to 13 contain months Jan to Dec
+temp_81to10 <- unlist(temp_all_months[, 2:13])
+
+paste("proportion smaller than p30")
+
+# Show proportion < than p30
+temp <- temp_81to10 < p30
+mean(temp)
+
+paste("Proportion larger than p70")
+
+# Show proportion > then p70
+mean(temp_81to10 > p70)
